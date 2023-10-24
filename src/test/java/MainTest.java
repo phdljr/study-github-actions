e03d6011ada83af7624fee10eb9518952c6ad90f
phdljr <phdljr@naver.com>
2023-10-24 10:27:31 +0900

    test: test gradle cache

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -5,8 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MainTest {
     @Test
     public void test(){
-        String str = "test";
+        String str = "test!";
 
-        assertThat(str).isEqualTo("test");
+        assertThat(str).isEqualTo("test!");
     }
 }
