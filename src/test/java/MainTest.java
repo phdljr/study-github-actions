4146a1c55459340955a38ee106c55fd3ea200e37
phdljr <phdljr@naver.com>
2023-10-24 10:18:47 +0900

    test: add failure testcase

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -9,4 +9,11 @@ public class MainTest {
 
         assertThat(str).isEqualTo("test");
     }
+
+    @Test
+    public void failTest(){
+        String str = "test";
+
+        assertThat(str).isEqualTo("test1");
+    }
 }
