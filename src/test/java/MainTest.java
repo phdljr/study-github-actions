8fc6987e225e0196081e654e2172c36300595891
phdljr <phdljr@naver.com>
2023-10-24 10:17:52 +0900

    test: add testcase

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MainTest {
+    @Test
+    public void test(){
+        String str = "test";
+
+        assertThat(str).isEqualTo("test");
+    }
+}
